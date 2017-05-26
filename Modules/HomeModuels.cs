d2f32f19496d7be45eefb694b13e89ea031c307d
JordLoop <jordloop@gmail.com>
2017-05-26 11:10:58 -0700

    add form to new-contact-form.cshtml, HomeModuel's ability to collects user input, Razor's ability to displays user input on new-contact-confirmed.cshtml.

--- Modules/HomeModuels.cs
+++ Modules/HomeModuels.cs
@@ -11,10 +11,15 @@ namespace AddressBook
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
+      Get["/contact/create"] = _ => {
+        return View["new-contact-form.cshtml"];
+      };
+      Post["/contact/new"] = _ => {
+        Contact newContact = new Contact(Request.Form["contactFirstName"], Request.Form["contactLastName"], Request.Form["contactAddress"], Request.Form["contactPhone"]);
+        List<Contact> allContacts = Contact.GetAllContacts();
+        return View["new-contact-confirmed.cshtml", newContact];
+      };
 
-
-
-      
     }
   }
 }
